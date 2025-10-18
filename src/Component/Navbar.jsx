a8fa7129c701c2bb353510387ae210b8c93919b1
Arju-Hasan <rajuahmeed698@gmail.com>
2025-10-18 12:28:55 +0600

    add navbar to navlink

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import { NavLink } from 'react-router';
+import  userProfile from '../assets/user.png'
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>My News App</h1>
-            <nav>
-                <ul>
-                    <li>Home</li>
-                    <li>Latest News</li>
-                    <li>Categories</li>
-                </ul>
-            </nav>
+        <div className='flex justify-between items-center py-4'>
+            <div></div>
+            <div className="nav space-x-4">
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/career">Career</NavLink>
+            </div>
+            <div className="profile flex items-center gap-4">
+            <img src={userProfile} alt="profile" />
+            <button className='btn btn-primary px-8'>Login</button>
+            </div>
         </div>
     );
 };
